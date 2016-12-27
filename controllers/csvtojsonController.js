826066ef625f16ac42430fcf63628a96c6943bfc
pramod1792 <pramod1792@gmail.com>
2016-12-27 14:47:42 +0530

    minor changes

--- controllers/csvtojsonController.js
+++ controllers/csvtojsonController.js
@@ -1,8 +1,9 @@
 var http = require('https');
 var fs = require('fs');
-var jsonCsvArray = [];
-const tempCsvFilePath='./out.csv';
 const csv=require('csvtojson');
+var jsonCsvArray = [];
+
+
 
 var downloadAndConvert = function(url, dest, callback) {
   var file = fs.createWriteStream(dest);
@@ -12,7 +13,7 @@ var downloadAndConvert = function(url, dest, callback) {
 		console.log("FINISH");
 
 		csv()
-		.fromFile(tempCsvFilePath)
+		.fromFile(dest)
 		.on('data',(data)=>{
 			jsonCsvArray.push(JSON.parse(data));
 		})
@@ -20,20 +21,18 @@ var downloadAndConvert = function(url, dest, callback) {
 			console.log(jsonCsvArray);
 			console.log('end'+jsonCsvArray.length);
 			callback(null,jsonCsvArray);
-		})
-      //file.close(callback);  // close() is async, call cb after close completes.
+		});
     });
   }).on('error', function(err) { // Handle errors
-    console.log("errrrrrrrrrrrrrrrrrrrrrrrrrrr");
+    console.log("Error processing file"+err);
     fs.unlink(dest); // Delete the file async. (But we don't check the result)
-    if (callback) callback(err.message);
+    callback(err.message);
   });
 };
 
 
 exports.downloadAndConvert = downloadAndConvert;
-//download('https://sakai.unc.edu/access/content/group/3d1eb92e-7848-4f55-90c3-7c72a54e7e43/public/data/bycatch.csv','./out3.csv',function(){console.log("ASDFGHJK");});
 
 // /convert/csv/to/json?q=link of csv file
 //q=https://s3.amazonaws.com/ed-college-choice-public/Most+Recent+Cohorts+(Scorecard+Elements).csv
-//https://sakai.unc.edu/access/content/group/3d1eb92e-7848-4f55-90c3-7c72a54e7e43/public/data/bycatch.csv
+//q=https://sakai.unc.edu/access/content/group/3d1eb92e-7848-4f55-90c3-7c72a54e7e43/public/data/bycatch.csv
